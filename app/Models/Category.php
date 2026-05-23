50e3374667b36b53d7b9eaa19cd986401a3f3cb2
MYusrilM10 <yusrilmbrk1122@gmail.com>
2026-05-24 04:32:25 +0700

    Update menyesuaikan CRUD

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,13 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Carbon\Carbon;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'popularity', 'created_at'];
+    protected $fillable = ['name', 'slug', 'popularity', 'created_at'];
 
     protected $dates = ['created_at', 'updated_at'];
+
+    // Auto-generate slug from name
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->slug) {
+                $model->slug = Str::slug($model->name);
+            }
+        });
+
+        static::updating(function ($model) {
+            if ($model->isDirty('name')) {
+                $model->slug = Str::slug($model->name);
+            }
+        });
+    }
 
     public function events()
     {
